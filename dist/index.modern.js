c156657da1d1477708a988adcdce3549238d4e2f
leb2 <brendan@uncountable.com>
2024-11-01 06:54:51 -0400

    bumping ketcher version, see base repo

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -5446,7 +5446,7 @@ var zoom = {
 
 var openHelpLink = function openHelpLink() {
   var _window$open;
-  return (_window$open = window.open("https://github.com/epam/ketcher/blob/".concat("2.24.0-rc.1-unc30\n", "/documentation/help.md#ketcher-overview"))) === null || _window$open === void 0 ? void 0 : _window$open.focus();
+  return (_window$open = window.open("https://github.com/epam/ketcher/blob/".concat("2.24.0-rc.1-unc31\n", "/documentation/help.md#ketcher-overview"))) === null || _window$open === void 0 ? void 0 : _window$open.focus();
 };
 var help = {
   help: {
@@ -35295,8 +35295,8 @@ var KetcherBuilder = function () {
                 cleanup = initApp(element, appRoot, staticResourcesUrl, {
                   buttons: buttons || {},
                   errorHandler: errorHandler || null,
-                  version: "2.24.0-rc.1-unc30" ,
-                  buildDate: "2024-08-27T19:41:03" ,
+                  version: "2.24.0-rc.1-unc31" ,
+                  buildDate: "2024-11-01T10:54:33" ,
                   buildNumber: ''
                 }, structService, resolve, togglerComponent);
               });
