3beee5eca18f3509189086cd9d942038f32fadd4
Martin Mullins <martin@uncountable.com>
2025-01-28 11:42:07 -0500

    bumping ketcher version, see base repo

--- dist/index.js
+++ dist/index.js
@@ -5514,7 +5514,7 @@ var zoom = {
 
 var openHelpLink = function openHelpLink() {
   var _window$open;
-  return (_window$open = window.open("https://github.com/epam/ketcher/blob/".concat("2.24.0-rc.1-unc35\n", "/documentation/help.md#ketcher-overview"))) === null || _window$open === void 0 ? void 0 : _window$open.focus();
+  return (_window$open = window.open("https://github.com/epam/ketcher/blob/".concat("2.24.0-rc.1-unc36\n", "/documentation/help.md#ketcher-overview"))) === null || _window$open === void 0 ? void 0 : _window$open.focus();
 };
 var help = {
   help: {
@@ -35378,8 +35378,8 @@ var KetcherBuilder = function () {
                 cleanup = initApp(element, appRoot, staticResourcesUrl, {
                   buttons: buttons || {},
                   errorHandler: errorHandler || null,
-                  version: "2.24.0-rc.1-unc35" ,
-                  buildDate: "2024-12-13T16:20:27" ,
+                  version: "2.24.0-rc.1-unc36" ,
+                  buildDate: "2025-01-28T16:41:53" ,
                   buildNumber: ''
                 }, structService, resolve, togglerComponent);
               });
